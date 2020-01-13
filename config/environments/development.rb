ace8437ea880bf65a4d51ec41d73abdd1134d00d
jastaffen <jack.staffen@gmail.com>
2020-01-13 18:49:54 -0500

    ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #ngrok shit
+  config.hosts << "1b64719a.ngrok.io"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
