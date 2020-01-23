da87316798de8ebce9b2495d789290ace922f686
jastaffen <jack.staffen@gmail.com>
2020-01-23 02:50:50 -0500

    configure all votes route and backend handling

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,7 +9,8 @@ class Vote < ApplicationRecord
         vote_count_by_song = Hash.new(0)
         song_arr = Vote.all.pluck(:artist_name, :song_name)
         song_arr.each { |obj| vote_count_by_song[obj] ? vote_count_by_song[obj] += 1 : vote_count_by_song[obj] = 1 }
-        vote_count_by_song
+        new_hash = vote_count_by_song.select { |k, val| val > 1 }
+        new_hash
     end
     
 end
